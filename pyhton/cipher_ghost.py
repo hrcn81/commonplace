956a4257ed4e3a623947847aed70cf1edf775f37
Mayank Solanki <147153666+hrcn81@users.noreply.github.com>
2024-03-11 09:20:46 +0530

    Update cipher_ghost.py

--- pyhton/cipher_ghost.py
+++ pyhton/cipher_ghost.py
@@ -1,1 +1,69 @@
+'''
+Important Inbuilt Libraries
+
+1.	math
+2.	random
+3.	datetime
+4.	collection
+5.	string
+'''
+
+
+'''
+1.	math
+'''
+# import math
+#
+# print('x:')
+# x = 10.8
+# print(math.ceil(x))
+# print(math.floor(x))
+# print(math.trunc(x))
+#
+#
+# print('\ny:')
+# y = 3
+# print(math.exp(y))
+# print(math.log10(y))
+#
+#
+# print('\nz:')
+# z = 90
+# print(math.sin(z))
+# print(math.cos(z))
+# print(math.tan(z))
+#
+#
+# print('\nOthers:')
+# print(math.pi)
+# print(math.e)
+# print(math.factorial(10))
+
+
+
+
+
+'''
+2.	Random
+'''
+# import random
+
+# print(random.random())						# Generates random number between 0 to 1
+# print(random.randint(1,3))					# Generates random number from 1 to 3 both end included
+# print(random.choice([1,2,3,4,5]))				# Gives any random values from the list
+# print(random.sample([1, 2, 3, 4, 5, 6], 3))	# Gives 3 samples from list
+# print(random.sample([1,2,3,4,5,6],4))			# Gives 4 samples from list
+# print(random.uniform(1.2,1.4))				# Generates random nomber between given range
+
+
+
+# random.seed(10)								# Everytime generates same values
+# print(random.random())						# Generates random number between 0 to 1
+# print(random.randint(1,3))					# Generates random number from 1 to 3 both end included
+# print(random.choice([1,2,3,4,5]))				# Gives any random values from the list
+# print(random.sample([1, 2, 3, 4, 5, 6], 3))	# Gives 3 samples from list
+# print(random.sample([1,2,3,4,5,6],4))			# Gives 4 samples from list
+# print(random.uniform(1.2,1.4))
+
+
 
