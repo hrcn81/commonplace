a6a787db25cb071bbc188f8b9de52e0af6ffa778
Mayank Solanki <147153666+hrcn81@users.noreply.github.com>
2024-03-25 12:42:10 +0530

    Update cipher_ghost.py

--- Python/cipher_ghost.py
+++ Python/cipher_ghost.py
@@ -512,8 +512,8 @@ cgpa	package
 # 		print(self.val,end = ' ')
 # 		if self.right:
 # 			self.right.inOrder()
-# 
-# 
+#
+#
 # # post - LRN
 # r1 = Node(50)
 # r1.left = Node(51)
@@ -528,16 +528,56 @@ cgpa	package
 # r1.left.right.right.right = Node(51)
 # r1.left.right.right.right.left = Node(33)
 # r1.left.right.right.right.right = Node(1)
-# 
+#
 # r1.right = Node(2)
 # r1.right.right = Node(3)
 # r1.right.right.left = Node(8)
 # r1.right.right.right = Node(5)
 # r1.right.right.right.left = Node(7)
 # r1.right.right.right.left.right = Node(12)
-# 
+#
 # r1.postOrder()
 
 
-
-
+# # Program to show various ways to read and
+# # write data in a file.
+# file1 = open( "myfile.txt", "w" )
+# L = [ "This is Delhi \n", "This is Paris \n", "This is London \n" ]
+# 
+# # \n is placed to indicate EOL (End of Line)
+# file1.write( "Hello \n" )
+# file1.writelines( L )
+# file1.close()  # to change file access modes
+# 
+# file1 = open( "myfile.txt", "r+" )
+# 
+# print( "Output of Read function is " )
+# print( file1.read() )
+# print()
+# 
+# # seek(n) takes the file handle to the nth
+# # bite from the beginning.
+# file1.seek( 0 )
+# 
+# print( "Output of Readline function is " )
+# print( file1.readline() )
+# print()
+# 
+# file1.seek( 0 )
+# 
+# # To show difference between read and readline
+# print( "Output of Read(9) function is " )
+# print( file1.read( 9 ) )
+# print()
+# 
+# file1.seek( 0 )
+# 
+# print( "Output of Readline(9) function is " )
+# print( file1.readline( 9 ) )
+# 
+# file1.seek( 0 )
+# # readlines function
+# print( "Output of Readlines function is " )
+# print( file1.readlines() )
+# print()
+# file1.close()
