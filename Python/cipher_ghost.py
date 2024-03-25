f3b494f5185e867f2498c9fb77341bc4d22ca124
Mayank Solanki <147153666+hrcn81@users.noreply.github.com>
2024-03-25 12:45:27 +0530

    Update cipher_ghost.py

--- Python/cipher_ghost.py
+++ Python/cipher_ghost.py
@@ -539,46 +539,68 @@ cgpa	package
 # r1.postOrder()
 
 
+
+
+'''
+Writing to a file
+
+There are two ways to write in a file.
+
+write() : Inserts the string str1 in a single line in the text file.
+File_object.write(str1)
+writelines() : For a list of string elements, each string is inserted in the text file.Used to insert multiple strings at a single time.
+File_object.writelines(L) for L = [str1, str2, str3] 
+Reading from a file
+
+There are three ways to read data from a text file.
+
+read() : Returns the read bytes in form of a string. Reads n bytes, if no n specified, reads the entire file.
+File_object.read([n])
+readline() : Reads a line of the file and returns in form of a string.For specified n, reads at most n bytes. However, does not reads more than one line, even if n exceeds the length of the line.
+File_object.readline([n])
+readlines() : Reads all the lines and return them as each line a string element in a list.
+  File_object.readlines()
+Note: ‘\n’ is treated as a special character of two bytes 
+'''
 # # Program to show various ways to read and
 # # write data in a file.
 # file1 = open( "myfile.txt", "w" )
 # L = [ "This is Delhi \n", "This is Paris \n", "This is London \n" ]
-# 
+#
 # # \n is placed to indicate EOL (End of Line)
 # file1.write( "Hello \n" )
 # file1.writelines( L )
 # file1.close()  # to change file access modes
-# 
+#
 # file1 = open( "myfile.txt", "r+" )
-# 
+#
 # print( "Output of Read function is " )
 # print( file1.read() )
 # print()
-# 
+#
 # # seek(n) takes the file handle to the nth
 # # bite from the beginning.
 # file1.seek( 0 )
-# 
+#
 # print( "Output of Readline function is " )
 # print( file1.readline() )
 # print()
-# 
+#
 # file1.seek( 0 )
-# 
+#
 # # To show difference between read and readline
 # print( "Output of Read(9) function is " )
 # print( file1.read( 9 ) )
 # print()
-# 
+#
 # file1.seek( 0 )
-# 
+#
 # print( "Output of Readline(9) function is " )
 # print( file1.readline( 9 ) )
-# 
+#
 # file1.seek( 0 )
 # # readlines function
 # print( "Output of Readlines function is " )
 # print( file1.readlines() )
 # print()
 # file1.close()
-
