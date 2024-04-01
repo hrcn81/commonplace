4cadbb7717f3cc422f98a949d152886cd208e002
Mayank Solanki <147153666+hrcn81@users.noreply.github.com>
2024-04-01 08:42:05 +0530

    Update cipher_ghost.py

--- Python/cipher_ghost.py
+++ Python/cipher_ghost.py
@@ -1086,3 +1086,49 @@ Link of the Python Notebook : Click here
 # l1.display()
 
 
+
+
+'''
+Given an array A of n non negative numbers. The task is to find the first equilibrium point in an array. 
+Equilibrium point in an array is an index (or position) such that the sum of all elements before 
+that index is same as sum of elements after it.
+
+Note: Return equilibrium point in 1-based indexing. Return -1 if no such point exists. 
+
+Example 1:
+Input: 
+n = 5 
+A[] = {1,3,5,2,2} 
+Output: 
+3 
+Explanation:  
+equilibrium point is at position 3 as sum of elements before it (1+3) = sum of elements after it (2+2).
+
+Example 2:
+Input:
+n = 1
+A[] = {1}
+Output: 
+1
+Explanation:
+Since there's only element hence its only the equilibrium point.
+
+Your Task:
+The task is to complete the function equilibriumPoint() which takes the array and n as input parameters and returns the point of equilibrium.
+'''
+# class ghost:
+#     def equilibriumPoint(self, lst, num):
+#         lst = list(lst)
+#         print(lst)
+#         addition = sum(lst)
+#         left = 0
+#         for i in range(num - 1):
+#             addition -= lst[i]
+#             if left == addition:
+#                 return i + 1
+#             left += lst[i]
+#         return -1
+# 
+# list1 = {1,3,5,2,2}
+# g1 = ghost()
+# print(g1.equilibriumPoint(list1,5))
